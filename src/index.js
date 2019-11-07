5b467fc9c6ccbd96f04852f668f556c5e758d793
Erislandio <34255207+Erislandio@users.noreply.github.com>
2019-11-06 23:03:19 -0300

    Access-Control-Allow-Origin
    Access-Control-Allow-Origin

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 8000;
 const routes = require("./routes/routes");
 require("./database/db");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(port, () => {
