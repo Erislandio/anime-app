45c4ddc6bb7f4627f9e985d4cf753acf7ff89cdf
erislandio.soares <erislandiosoares21@gmail.com>
2019-12-06 08:21:39 -0300

    ajustes porta

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 8000 || process.env.PORT;
 const routes = require("./routes/routes");
 const cors = require("cors");
 const morgan = require("morgan");
 
 app.use(express.json());
-app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan());
 app.use(routes);
+app.use(cors());
+
 
 app.listen(port, () => {
   console.log("Running");
